5dda0d4bd2e9285bc872ec5577d8a1111df7b140
Zorgga <kesley.martins@vivaldi.net>
2019-09-06 18:57:34 -0300

    Só organizando mesmo

--- Estrutura de Dados I/Provas #01/Prova #003 - Z.c
+++ Estrutura de Dados I/Provas #01/Prova #003 - Z.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <time.h>
 
+// Em produção
+
 int main(){
 	int jog, com, winner, cont_jog=0, cont_comp=0;
 	int user[5], comp[5], aux;
@@ -14,7 +16,6 @@ int main(){
 		user[i] = 1 + rand() % 13;
 		comp[i] = 1 + rand() % 13;
 	}
-
 
 	// Laço principal do jogo
 	while(1){
@@ -61,85 +62,7 @@ int main(){
 		scanf(" %i",&jog);
 
 		// Escolhe a carta do computador
-		for(int x=0; x < 5; x++){
-			if(comp[x] > jog && comp[x] < 14){
-				com = comp[x];
-				break;
-			} else
-				com = comp[0];
-		}
 
 		printf("Carta jogada pelo computador: %i", com);
-
-		// Condição de ganhar a rodada e consequencias
-		if(jog > com){
-			printf("\n- - - - - - - - - - - - - - - - - - - -");
-			printf("\nVocê ganhou a rodada!\n");
-
-			// Converte a carta escolhida pelo usuário para 19
-			for(int x=0; x < 5; x++){
-				if(user[x] == jog){
-					user[x] = 19;
-					break;
-				}
-			}
-
-			// Randomiza uma nova carta para o computador
-			for(int x=0; x < 5; x++){
-				if(comp[x] == com){
-					comp[x] = 1 + rand() % 13;
-					break;
-				}
-			}
-
-		} else if(com > jog){
-			printf("\n- - - - - - - - - - - - - - - - - - - -");
-			printf("\nO computador ganhou a rodada!\n");
-
-			// Converte a carta escolhida pelo computador para 19
-			for(int x=0; x < 5; x++){
-				if(comp[x] == com){
-					comp[x] = 19;
-					break;
-				}
-			}
-
-			// Randomiza uma nova carta para o jogador
-			for(int x=0; x < 5; x++){
-				if(user[x] == jog){
-					user[x] = 1 + rand() % 13;
-					break;
-				}
-			}
-		}
-
-		for(int x=0; x < 5; x++){
-			if(user[x] == 19)
-				cont_jog++;
-
-			if(comp[x] == 19)
-				cont_comp++;
-		}
-
-		if(cont_jog == 5){
-			winner = 1;
-			break;
-		}
-
-		if(cont_comp == 5){
-			winner = 0;
-			break;
-		}
-	}
-
-	if(winner == 1){
-		printf("\n\n- - - - - - - - - - - - - - - - - - - -\n");
-		printf("O jogador venceu o jogo!\n");
-		printf("- - - - - - - - - - - - - - - - - - - -\n");
-	} else {
-		printf("\n\n- - - - - - - - - - - - - - - - - - - -\n");
-		printf("O computador venceu o jogo!\n");
-		printf("- - - - - - - - - - - - - - - - - - - -\n");
-	}
 
 }
