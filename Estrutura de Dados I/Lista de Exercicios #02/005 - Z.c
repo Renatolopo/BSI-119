ab3d72d5c5efe76fdcd4d135db18c2ff77c7f70a
kesleymartins <kesley.martins@vivaldi.net>
2019-10-05 04:16:48 -0300

    Organização do repositório

--- Estrutura de Dados I/Lista de Exercicios #02/005 - Z.c
+++ Estrutura de Dados I/Lista de Exercicios #02/005 - Z.c
@@ -1,34 +1,70 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-// aaaaaaaaaaaa tnc desse karaio
-
-int main(){
+int main()
+{
 	int x, y;
 
-	printf("Insira o tamanho do vetor X: ");
-	scanf(" %i",& x);
-
-	printf("Insira o tamanho do vetor Y: ");
-	scanf(" %i",& y);
-
-	int vx[x], vy[y], aux;
+	printf("informe X: ");
+	scanf(" %i", &x);
+	printf("informe Y: ");
+	scanf(" %i", &y);
 
 	srand(time(NULL));
+	
+	int vetx[x], vety[y];
+	int c, num, cont; 
+	
+	// Randomiza tipos inteiros não repetidos Vetx
+	for(int i=0; i<x; i++){
+		vetx[i] = rand()% x+y+1;
 
-	for(int i=0; i < x; i++){
-		vx[i] = rand() % (x+y+1);
+		while(1){
+			cont = 0;
 
-		for(int j=0; j < i; j++){
-			if(vx[j] == vx[i]){
-				vx[j] = rand() % (x+y+1);
-				j=0;
+			for(c=0;c<i;c++)
+				if(vetx[i] == vetx[c])
+					cont++;
+
+			if(cont == 0){
+				break;
 			}
+			
+			vetx[i] = rand()% x+y+1;
 		}
 	}
 
-	for (int i = 0; i < x; ++i){
-		printf("%i\n", vx[i]);
+	// Randomiza tipos inteiros não repetidos Vety
+	for(int i=0; i<y; i++){
+		vety[i] = rand() % x+y+1;
+
+		while(1){
+			cont = 0;
+
+			for(c=0; c<i; c++)
+				if(vety[i] == vety[c])
+					cont++;
+
+			if(cont == 0){
+				break;
+			}
+
+			vety[i] = rand() % x+y+1;
+		}
 	}
+
+	// Mostra os valores de vetx  e vety
+	printf("Vetor X: ");
+	for(int i=0; i<x; i++)
+		printf("%i ", vetx[i]);
+
+	printf("\n");
+
+	printf("Vetor Y: ");
+	for(int i=0; i<y; i++)
+		printf("%i ", vety[i]);
+	
+	// 
+	printf("\nInterseção de X e Y");
 }
