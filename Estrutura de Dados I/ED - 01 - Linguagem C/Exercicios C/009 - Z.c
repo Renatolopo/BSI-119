8d546d5c61b66285a52da5ca63e829dee7436a45
kesleymartins <kesley.martins@vivaldi.net>
2019-10-07 15:54:14 -0300

    Organização do repositório

--- Estrutura de Dados I/ED - 01 - Linguagem C/Exercicios C/009 - Z.c
+++ Estrutura de Dados I/ED - 01 - Linguagem C/Exercicios C/009 - Z.c
@@ -6,10 +6,8 @@ int main(){
 	printf("Insira o tempo de evento em segundos: ");
 	scanf(" %i",&tempo);
 
-	// Divide a quantidade total de segundos pela quantidade total de segundos em um hora
 	hora = tempo / 3600;
 	
-	// diminui a quantidade de segundos por hora pelo total de segundos e divide por 60
 	min = (tempo - hora * 3600) / 60; 
 
 	seg = tempo - (min * 60 + hora * 3600);
